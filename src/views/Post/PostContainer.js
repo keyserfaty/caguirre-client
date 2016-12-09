e0efa1130f2a2de73b75f6f3cc61ea732612ddff
keyserfaty <keserfaty@gmail.com>
2016-12-09 15:13:36 -0300

    add dinamic reading bar

--- src/views/Post/PostContainer.js
+++ src/views/Post/PostContainer.js
@@ -3,6 +3,27 @@ import Post from './Post'
 import Footer from '../../common/Footer'
 import Aside from '../../common/Aside'
 
+let header
+
+window.addEventListener('load', () => {
+  const d = document
+  header = d.querySelector('header') //* Ugly but works
+})
+
+let readBarWidth = (scrollPosition, windowHeight) => {
+  if (windowHeight > 0) {
+    return scrollPosition * 100 / windowHeight
+  } else {
+    return 0
+  }
+}
+
+window.addEventListener('scroll', () => {
+  const fullHeight = document.body.scrollHeight - document.body.clientHeight
+  const scrollPosition = window.pageYOffset
+  header.setAttribute('style', 'width: ' + readBarWidth(scrollPosition, fullHeight) + '%')
+})
+
 const Container = props => (
   <span>
     <header />
