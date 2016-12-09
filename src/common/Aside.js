8cca29719559979bd6fedb0f2e4a2b4ee7ee1533
keyserfaty <keserfaty@gmail.com>
2016-12-09 14:18:09 -0300

    add go to top on click

--- src/common/Aside.js
+++ src/common/Aside.js
@@ -1,9 +1,20 @@
 import h from 'vhtml'
 
+window.addEventListener('load', () => {
+  const d = document
+  const top = d.querySelector('.top') //* Ugly but works
+})
+
+top.onclick = () => {
+  window.scrollTo(0, 0)
+}
+
 const Aside = prop => (
   <aside>
     <div class="top"></div>
-    <div class="menu"></div>
+    <a href="#/posts">
+      <div class="menu"></div>
+    </a>
   </aside>
 )
 
