9bfa096a1e20f105162e405fd62b72679932c866
keyserfaty <keserfaty@gmail.com>
2016-12-10 15:51:31 -0300

    [#4] add link to @aguirrecaro

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -2,7 +2,7 @@ import h from 'vhtml'
 
 const Footer = props => (
   <footer>
-    <span class="author">Por: <strong>Carolina Aguirre</strong></span>
+    <span class="author">Por: <strong><a href="http://www.twitter.com/aguirrecaro">Carolina Aguirre</a></strong></span>
     <span class="creator">Creado con ❤️ por <a href="http://www.twitter.com/keyserfaty">@keyserfaty</a></span>
   </footer>
 )
